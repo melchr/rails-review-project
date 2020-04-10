8101c90e9db17de251147f513ac683c4c7067114
Mel <melchrstmn@gmail.com>
2020-04-09 20:57:05 -0400

    added action

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,14 @@
 class AlbumsController < ApplicationController
+
+    def index
+        @albums = Album.all
+    end
+
+    def show
+
+    end
+
+    def new
+        @album = Album.new
+    end
 end
