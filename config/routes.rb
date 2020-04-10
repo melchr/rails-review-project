466cd60bcb66f6a1945bdfe677b93c4dca031af1
Mel <melchrstmn@gmail.com>
2020-04-09 22:33:58 -0400

    added special routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get '/signup' => 'users#new', as: 'signup'
+  post '/signup' => 'users#create'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
   resources :comments
   resources :reviews
   resources :albums
