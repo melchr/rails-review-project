9ba1649fb9e5981ae2b1383a9a2f136443d5b175
Mel <melchrstmn@gmail.com>
2020-04-09 20:58:48 -0400

    edited root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :reviews
   resources :albums
   resources :users
+
+  root to: "albums#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
