5df4471a95d88921cdd553639d46d47fb9607981
Mel <melchrstmn@gmail.com>
2020-04-11 00:36:38 -0400

    added create, show actions and params

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -6,10 +6,25 @@ class AlbumsController < ApplicationController
     end
 
     def show
-
+        @album = Album.find(params[:id])
     end
 
     def new
         @album = Album.new
+    end
+
+    def create
+        @album = Album.new(album_params)
+        if @album.save
+            redirect_to album_path(@album)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def album_params
+        params.require(:album).permit(:artist, :title)
     end
 end
