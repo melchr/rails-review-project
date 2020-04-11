d2efff0ad15eda7c52e24e09b100f8210a68295e
Mel <melchrstmn@gmail.com>
2020-04-10 21:10:23 -0400

    sessions weren't working properly bc forgot bcrypt, installed bcrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,16 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if params[:user]
-            @user = User.find_by(email: params[:user][:email])
-            if @user && @user.authenticate(params[:user][:password])
-                session[:user_id] = @user.id
-                redirect_to user_path(@user)
-            else
-                redirect_to signin_path
-            end
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+         session[:user_id] = user.id
+         #flash[:success] = Successfully Logged In!
+         redirect_to user_path
+        else
+         #flash[:warning] = “Invalid Username or Password”
+         redirect_to signin_path
         end
-    end
+       end
 
     def destroy
         session.clear
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :reviews
     has_many :albums, through: :reviews  #<<< understand this
 end
