217f838b624394d93a2781923bcb7949f4496094
Mel <melchrstmn@gmail.com>
2020-04-11 00:37:52 -0400

    added accepts nested attributes method

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+    accepts_nested_attributes_for :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     has_many :reviews
     has_many :albums, through: :reviews  #<<< understand this
+    accepts_nested_attributes_for :reviews
 end
