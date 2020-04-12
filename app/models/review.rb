8c5e0b8a91f4c35539f384415f1fdd1ae79f5b9b
Mel <melchrstmn@gmail.com>
2020-04-12 13:41:26 -0400

    added optional true to album relationship

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
-    belongs_to :album
+    belongs_to :album, optional: true #<<<< i think this line saved my life
     belongs_to :user
+    accepts_nested_attributes_for :album
 end
