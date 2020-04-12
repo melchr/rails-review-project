d50a2705833e7232768713809afbcc7caed44009
Mel <melchrstmn@gmail.com>
2020-04-12 00:10:50 -0400

    fixed create action but still not saving

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,32 @@
 class ReviewsController < ApplicationController
 
+    def index
+        @reviews = Review.all
+    end
+
     def show
-        @reviews = Review.where("album_id = ?", params[:album_id])
+        @review = Review.find(params[:id])
+        #@reviews = Review.where("album_id = ?", params[:album_id])
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @album = Album.find(params[:album_id])
+        current_user.reviews.create(review_params)
+        redirect_to @album
+    end
+
+    def update
+
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:title, :date, :content)
     end
 
 end
