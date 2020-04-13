874dbe8c2bdcf9676743ea926dfbc0292b87eb4e
Mel <melchrstmn@gmail.com>
2020-04-12 23:01:14 -0400

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    validates_presence_of :password, :on => :create
+    validates_format_of :name, :with => /[A-Za-z]+/, :on => :create
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+    validates_length_of :password, :minimum => 5, :on => :create
     has_many :reviews
     has_many :albums, through: :reviews  #<<< understand this
     accepts_nested_attributes_for :reviews
