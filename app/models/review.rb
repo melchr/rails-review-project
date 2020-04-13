9bc3afb2763f7fad405df18ac2a332a9f79c374b
Mel <melchrstmn@gmail.com>
2020-04-12 23:01:56 -0400

    testing, commented out

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
+    #attr_accessible :title, :date, :content, :user_id, :album_id
     belongs_to :album, optional: true #<<<< i think this line saved my life
     belongs_to :user
     accepts_nested_attributes_for :album
