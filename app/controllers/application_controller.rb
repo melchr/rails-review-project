8f9675d2091f892087116e4c43afd3e593494cbe
Mel <melchrstmn@gmail.com>
2020-04-12 22:59:54 -0400

    edited current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :logged_in?
 
+    private
+
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
     def logged_in?
