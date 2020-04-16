53733b731a25364a3dc7bdc1faecfa9d69e357ad
Mel <melchrstmn@gmail.com>
2020-04-16 18:14:06 -0400

    uninstalled boostrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "bootstrap"
-import "../stylesheets/application"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
