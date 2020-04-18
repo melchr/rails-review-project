f41b1286f60400f896eccf7f10cd8632b3e4c7d0
Mel <melchrstmn@gmail.com>
2020-04-18 13:13:05 -0400

    added new before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,20 @@ class ApplicationController < ActionController::Base
 
     private
 
+    
+
     def current_user
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
     def logged_in?
         !!session[:user_id]
+    end
+
+    def must_login
+        if !logged_in?
+            redirect_to signin_path
+        end
     end
 
 end
