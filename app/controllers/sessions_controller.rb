ff28be505ee1ed145948993e2e1ed40f83a3c67d
Mel <melchrstmn@gmail.com>
2020-04-18 13:20:41 -0400

    fixed login issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,23 +2,23 @@ class SessionsController < ApplicationController
     def new
         @user = User.new
     end
-
     def create
-        @message = params[:password].empty? || params[:email].empty? ? "Please include your Email & Password" : nil
-        if params[:email]
-            @user = User.find_by(email: params[:email])
+        unless params[:email].empty?
+            @user = User.find_by_email(params[:email])
             if @user && @user.authenticate(params[:password])
                 session[:user_id] = @user.id
                 redirect_to albums_path
             else
+                flash[:alert] = "Email or Password Incorrect.  Please try again."
                 redirect_to signin_path
             end
+        else
+            flash[:alert] = "Please include your Email & Password."
+            redirect_to signin_path
         end
     end
-
     def destroy
         session.clear
         redirect_to root_path
     end
-
 end
