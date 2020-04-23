d29bfa4669ce93566f0f79b820715e777e5b7dd4
Mel <melchrstmn@gmail.com>
2020-04-22 21:50:00 -0400

    small change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
 
     private
 
-    
-
     def current_user
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
