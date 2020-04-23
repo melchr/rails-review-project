3772a90a898927570d6c0dc4e84148c6daa9b3df
Mel <melchrstmn@gmail.com>
2020-04-22 21:51:22 -0400

    added destroy for review, custom validation

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,13 +1,18 @@
-class Album < ApplicationRecord
-    has_many :reviews
+class Album < ApplicationRecord #model in logic is re-usable, represents data to controller
+    has_many :reviews, :dependent => :destroy
     has_many :users, through: :reviews
     has_one_attached :avatar
     accepts_nested_attributes_for :reviews
     validates_presence_of :artist
-    validates_presence_of :title
-    scope :with_recent_reviews, -> { includes(:reviews).where(reviews: { date: [(Date.today - 7.days)..Date.tomorrow] }) } #scope relies on include method and custom query on related model (reviews)
+    validate :unique_title_by_artist
+    scope :with_recent_reviews, -> { includes(:reviews).where(reviews: { date: [(Date.today - 7.days)..Date.tomorrow] }) } #scope relies on include method and custom query on related model (reviews) 
 
-    #def user_attributes=(user_attributes) 
-    #    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
-    # end 
+    private
+
+    def unique_title_by_artist #changes to downcase, checks if title already exists
+        if Album.where('lower(title) = ? AND artist = ?', self.title.downcase, self.artist).count > 0
+          error.add(:title, "must be unique to artist")
+        end
+    end
+
 end
